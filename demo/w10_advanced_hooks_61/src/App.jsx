45af809595aebe14f3810d57eea75b664490216e
haowei <212410061@0365.tku.edu.tw>
2024-11-20 19:41:21 +0800

    W11-P1: Conditonal rendering, show two cases: error and fetch data

---
+++ demo/w10_advanced_hooks_61/src/App.jsx
@@ -0,0 +1,14 @@
+import T11_61 from './tutorial/01-useState/T11-error-example'
+import T12_61 from './tutorial/01-useState/T12-error-example'
+import T21_61 from './tutorial/02-useEffect/T21-code-example'
+function App() {
+  return (
+    <div className='container'>
+      <h4>Tutorial T1_11</h4>
+      <h5>周浩偉 212410061</h5>
+      <T21_61 />
+    </div>
+  )
+}
+
+export default App
